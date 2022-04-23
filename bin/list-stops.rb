bfa273c260b6c77b6eaa618b1bc64957d1a261aa
digitalronin <david@digitalronin.com>
2022-04-23 12:45:49 +0900

    Call the API to get nearest bus stop to a location

--- bin/list-stops.rb
+++ bin/list-stops.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 require "json"
+require "net/http"
+require "uri"
 require "debug"
 
 class BusStop
@@ -18,14 +20,38 @@ class BusStop
   end
 end
 
-json = File.read("stoppoints.json")
+class Location
+  attr_reader :lat, :lon
 
-data = JSON.parse(json)
+  def initialize(hash)
+    @lat = hash.fetch(:lat)
+    @lon = hash.fetch(:lon)
+  end
 
-stops = data.fetch("stopPoints")
-  .map { |hash| BusStop.new(hash) }
-  .sort {|a,b| a.distance <=> b.distance}
+  def nearest_bus_stops
+    Api.stop_points(lat: lat, lon: lon)
+      .map { |hash| BusStop.new(hash) }
+      .sort {|a,b| a.distance <=> b.distance}
+  end
+end
+
+class Api
+  def self.stop_points(lat:, lon:)
+    url = "https://api.tfl.gov.uk/StopPoint/?lat=#{lat}&lon=#{lon}&stopTypes=NaptanPublicBusCoachTram&radius=250&modes=bus"
+    json = Net::HTTP.get(URI(url))
+    data = JSON.parse(json)
+    data.fetch("stopPoints")
+  end
+end
+
+lat = 51.5000182
+lon = -0.1347679
+
+location = Location.new({lat: lat, lon: lon})
+
+stops = location.nearest_bus_stops
 
 debugger
 
-pp data
+puts "foo"
+
